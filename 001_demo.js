ec18b4569a3ec3645304a3214df3dfd5b1204e9c
Rishi <alluririshivarma@gmail.com>
2024-02-29 22:09:26 +0530

    29-feb

--- 001_demo.js
+++ 001_demo.js
@@ -11,56 +11,63 @@
 // console.log(regrex.test(string1));
 // console.log(string1.split("").sort().join(""));
 
-const transaction = [
-    {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: "food",
-		itemName: 'Pizza',
-	},
-    {
-		id: 2,
-		timestamp: 1656076800000,
-		price: 20,
-		category: 'cloth',
-		itemName: 'Pizza1',
-	},
-    {
-		id: 3,
-		timestamp: 1656076800000,
-		price: 30,
-		category: "food",
-		itemName: 'Pizza1',
-	}
-]
+// const transaction = [
+//     {
+// 		id: 1,
+// 		timestamp: 1656076800000,
+// 		price: 10,
+// 		category: "food",
+// 		itemName: 'Pizza',
+// 	},
+//     {
+// 		id: 2,
+// 		timestamp: 1656076800000,
+// 		price: 20,
+// 		category: 'cloth',
+// 		itemName: 'Pizza1',
+// 	},
+//     {
+// 		id: 3,
+// 		timestamp: 1656076800000,
+// 		price: 30,
+// 		category: "food",
+// 		itemName: 'Pizza1',
+// 	}
+// ]
 
-function checkcat(ans,cat){
-   return  ans.findIndex( (item) => item.category === cat);
-}
-function add(ans,idx,cat,price1){
-    if(idx==-1){ // not present in ans    
-        let obj1 = {
-            category : cat ,
-            moneySpent : price1 
-        }
-        ans.push(obj1);
-    }
-    else{
-        ans[idx].moneySpent += price1 ;  
-    }
-}
+// function checkcat(ans,cat){
+//    return  ans.findIndex( (item) => item.category === cat);
+// }
+// function add(ans,idx,cat,price1){
+//     if(idx==-1){ // not present in ans    
+//         let obj1 = {
+//             category : cat ,
+//             moneySpent : price1 
+//         }
+//         ans.push(obj1);
+//     }
+//     else{
+//         ans[idx].moneySpent += price1 ;  
+//     }
+// }
 
-   const ans  = [] ;
-   transaction.forEach( (item)=>
-   { 
-   let cat =  item.category ; 
-   let catIndex = checkcat(ans,cat);
-   add(ans,catIndex,cat,item.price);
-   }
-   )
-   console.log(ans);
+//    const ans  = [] ;
+//    transaction.forEach( (item)=>
+//    { 
+//    let cat =  item.category ; 
+//    let catIndex = checkcat(ans,cat);
+//    add(ans,catIndex,cat,item.price);
+//    }
+//    )
+//    console.log(ans);
      
      
 
  
+let str1 = "rishi";
+let str2 = "ishir";
+
+let y = str1.split("").sort(); 
+let x = str2.split("").sort();
+console.log(y);
+console.log(x);
